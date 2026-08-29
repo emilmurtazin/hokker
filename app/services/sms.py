b680258dd5d343f86ac61c752f3bed5cf1a18ce6
emilmurtazin <80699714+emilmurtazin@users.noreply.github.com>
2026-08-29 12:46:19 +0300

    Update sms.py

--- app/services/sms.py
+++ app/services/sms.py
@@ -80,7 +80,8 @@ class SMSCProvider(SMSProvider):
     def send(self, phone: str, code: str) -> None:
         params = {
             "phones": phone,
-            "mes": f"Код подтверждения ХОККЕР: {code}",
+            "mes": f"Код подтверждения {code}",
+            "sender": "+79872311693",
             "fmt": 3,  # JSON-ответ
             "charset": "utf-8",
         }
